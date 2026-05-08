617b6e039b41d3cb5e2d6affbe1627c8ca21b5dd
kristianoveckin-png <kristianoveckin@gmail.com>
2026-05-08 18:09:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, send_file, send_from_directory
 import yt_dlp
 import os
 
 app = Flask(__name__)
+
+# Маршрут для иконки сайта
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(".", "favicon.png")
 
 @app.route('/')
 def index():
@@ -12,25 +17,20 @@ def index():
 def download():
     video_url = request.form.get('url')
     
-    # Настройки для yt-dlp
+    # Настройки для yt-dlp (используем /tmp/ для Render)
     ydl_opts = {
-        'format': 'best', # Скачивать лучшее доступное качество
-        'outtmpl': 'downloads/%(title)s.%(ext)s', # Папка для временного хранения
+        'format': 'best', 
+        'outtmpl': '/tmp/%(title)s.%(ext)s', 
     }
 
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            # Извлекаем информацию о видео
             info = ydl.extract_info(video_url, download=True)
             filename = ydl.prepare_filename(info)
             
-            # Отправляем файл пользователю
             return send_file(filename, as_attachment=True)
     except Exception as e:
         return f"Ошибка при скачивании: {e}"
 
 if __name__ == '__main__':
-    # Создаем папку downloads, если её нет
-    if not os.path.exists('downloads'):
-        os.makedirs('downloads')
     app.run(debug=True)
